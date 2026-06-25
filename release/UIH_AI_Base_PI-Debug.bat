f363a1741ccc232a3658713b3b50d91cbb742aca
bingjian999 <bingjian.wang@united-imaging.com>
2026-06-25 01:54:54 -0500

    fix: update Debug BAT to reference v1.0.4 EXE

--- release/UIH_AI_Base_PI-Debug.bat
+++ release/UIH_AI_Base_PI-Debug.bat
@@ -14,7 +14,7 @@ echo ------------------------------------------------------------
 echo.
 
 REM Run the EXE and capture exit code
-"%~dp0UIH_AI_Base_PI-v1.0.3.exe" 2>&1
+"%~dp0UIH_AI_Base_PI-v1.0.4.exe" 2>&1
 set EXITCODE=%ERRORLEVEL%
 
 echo.
